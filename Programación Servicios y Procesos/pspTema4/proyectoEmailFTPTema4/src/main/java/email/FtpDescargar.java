4b5657813021104150ef63a4b41230f12e9eb145
Antonnioro <50053892+Antonnioro@users.noreply.github.com>
2025-02-16 14:13:12 +0100

    Add files via upload

---
+++ Programación Servicios y Procesos/pspTema4/proyectoEmailFTPTema4/src/main/java/email/FtpDescargar.java
@@ -0,0 +1,58 @@
+package email;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class FtpDescargar {
+	public static void main(String[] args) {
+
+		FTPClient cliente = new FTPClient();
+		String ftp = "demo.wftpserver.com";
+		String user = "demo";
+		String password = "demo";
+		int port = 21;
+
+		try {
+			System.out.println("Conectando al servidor FTP...");
+			cliente.connect(ftp, port);
+			boolean login = cliente.login(user, password);
+
+			if (login) {
+				System.out.println("Conexión establecida con el servidor FTP");
+
+				cliente.setFileType(FTP.BINARY_FILE_TYPE);
+				cliente.setFileTransferMode(FTP.BINARY_FILE_TYPE);
+				cliente.enterLocalPassiveMode();
+
+				File fichero = new File("Winter.jpg");
+				OutputStream salida = new BufferedOutputStream(new FileOutputStream(fichero));
+
+				System.out.println("Espera mientras se realiza la descarga....");
+				boolean ok = cliente.retrieveFile("download/Winter.jpg", salida);
+
+				salida.close();
+
+				if (ok) {
+					System.out.println("La descarga ha finalizado con éxito");
+				} else {
+					System.out.println("La descarga no se ha realizado");
+				}
+
+				cliente.logout();
+				cliente.disconnect();
+			} else {
+				System.out.println("Conexión no establecida con el servidor FTP");
+			}
+		} catch (IOException e) {
+			System.out.println("No se ha podido descargar el fichero");
+			System.out.println("Error: " + e.getMessage());
+		}
+
+	}
+
+}
